33bc5007e119cd67a841b46c01760c845300dd65
Hectortdn <hector.kayque012@gmail.com>
2025-01-04 23:17:31 -0300

    chore: prepare deploy

--- api_rest/env/index.ts
+++ api_rest/env/index.ts
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
-  DATABASE_CLIENT: z.string(),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
--- api_rest/src/database.ts
+++ api_rest/src/database.ts
@@ -1,11 +1,14 @@
 import { Knex, knex as setupKnex } from 'knex'
 import { env } from '../env'
 
+const databaseConnection =
+  env.DATABASE_CLIENT === 'pg'
+    ? env.DATABASE_URL
+    : { filename: env.DATABASE_URL }
+
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  connection: databaseConnection,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
